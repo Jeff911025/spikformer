adbb5971117d58d5450d989f1e7c38be81295859
Jeff0102030433 <Jeff0102030433@example.com>
2025-07-08 14:07:42 +0800

    add the env.sh haha

---
+++ env.sh
@@ -0,0 +1,4 @@
+pip install torch==1.10.0+cu111 torchvision==0.11.1+cu111 torchaudio==0.10.0 --extra-index-url https://download.pytorch.org/whl/cu111
+pip install cupy-cuda12x
+pip install timm==0.5.4 pyyaml
+pip install git+https://github.com/fangwei123456/spikingjelly.git@0.0.0.0.12
